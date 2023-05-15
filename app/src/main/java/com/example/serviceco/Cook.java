c146df0f03068460f0565c04760927319f4f9191
Ayush <a13yush@gmail.com>
2023-05-15 15:39:38 +0530

    cook

--- app/src/main/java/com/example/serviceco/Cook.java
+++ app/src/main/java/com/example/serviceco/Cook.java
@@ -4,12 +4,9 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
-import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
-import android.annotation.SuppressLint;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.res.Resources;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
@@ -27,43 +24,26 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 import com.squareup.picasso.Picasso;
 
-public class Home extends AppCompatActivity {
-
-    SwipeRefreshLayout swipeRefreshLayout;
+public class Cook extends AppCompatActivity {
 
     private TextView textViewWelcome;
-    private ProgressBar progressBar;
     private String fullName;
-    private CardView cook, maid, delivery, labour, babysitter;
     private ImageView imageView;
     private FirebaseAuth authProfile;
-    private SwipeRefreshLayout swipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_cook);
 
         getSupportActionBar().hide();
-        swipetoLoad();
         textViewWelcome = findViewById(R.id.textView_show_welcome);
-        progressBar = findViewById(R.id.progress_bar);
 
-        //Set on click listener on ImageView to open upload profile picture
         imageView = findViewById(R.id.imageView_profile_dp);
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(Home.this, UserProfileActivity.class);
-                startActivity(intent);
-            }
-        });
-
-        cook = findViewById(R.id.cook);
-        cook.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(Home.this, Cook.class);
+                Intent intent = new Intent(Cook.this, UserProfileActivity.class);
                 startActivity(intent);
             }
         });
@@ -72,33 +52,14 @@ public class Home extends AppCompatActivity {
         FirebaseUser firebaseUser = authProfile.getCurrentUser();
 
         if(firebaseUser==null){
-            Toast.makeText(Home.this, "Something went wrong! User's details are not available at the moment",Toast.LENGTH_LONG).show();
+            Toast.makeText(Cook.this, "Something went wrong! User's details are not available at the moment",Toast.LENGTH_LONG).show();
         }
         else{
             checkifEmailVerification(firebaseUser);
-            progressBar.setVisibility(View.VISIBLE);
             showUserProfile(firebaseUser);
         }
-        
     }
 
-    private void swipetoLoad() {
-        swipeContainer = findViewById(R.id.swipeContainer);
-
-        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
-            @Override
-            public void onRefresh() {
-                startActivity(getIntent());
-                finish();
-                overridePendingTransition(0,0);
-                swipeContainer.setRefreshing(false);
-            }
-        });
-
-        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright, android.R.color.holo_green_light, android.R.color.holo_orange_light, android.R.color.holo_red_light);
-    }
-
-    //User coming to profile after successful registration
     private void checkifEmailVerification(FirebaseUser firebaseUser) {
         if(!firebaseUser.isEmailVerified()){
             showAlertDialog();
@@ -107,7 +68,7 @@ public class Home extends AppCompatActivity {
 
     private void showAlertDialog() {
         //Setup alert builder
-        AlertDialog.Builder builder = new AlertDialog.Builder(Home.this);
+        AlertDialog.Builder builder = new AlertDialog.Builder(Cook.this);
         builder.setTitle("Email not Verified");
         builder.setMessage("Please verify your email now. You can not login without verification");
 
@@ -138,25 +99,20 @@ public class Home extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 ReadWriteUserDetails readUserDetails = snapshot.getValue(ReadWriteUserDetails.class);
-                if(readUserDetails!=null){
+                if (readUserDetails != null) {
                     fullName = firebaseUser.getDisplayName();
-                    StringBuilder sb=new StringBuilder();
+                    StringBuilder sb = new StringBuilder();
                     int unicode = 0x1F60A;
                     String s = String.valueOf(Character.toChars(unicode));
-                    int i=0;
-                    while(sb.length() <9 && fullName.charAt(i)!=' ')
-                    {
+                    int i = 0;
+                    while (sb.length() < 9 && fullName.charAt(i) != ' ') {
                         sb.append(fullName.charAt(i));
                         i++;
                     }
-                    if(sb.length()==9)
-                    {
+                    if (sb.length() == 9) {
                         textViewWelcome.setText(sb.toString());
-                    }
-                    else
-                    {
-                        for(i=sb.length();i<10;i++)
-                        {
+                    } else {
+                        for (i = sb.length(); i < 10; i++) {
                             sb.append(" ");
                         }
                         textViewWelcome.setText(sb.toString());
@@ -167,19 +123,15 @@ public class Home extends AppCompatActivity {
                     //Image Viewer setImageURI() should not be used with regular URIs. So we are using Picasso here
                     Picasso.get().load(uri).into(imageView);
 
+                } else {
+                    Toast.makeText(Cook.this, "Something went wrong!", Toast.LENGTH_LONG).show();
                 }
-                else{
-                    Toast.makeText(Home.this, "Something went wrong!",Toast.LENGTH_LONG).show();
-                }
-                progressBar.setVisibility(View.GONE);
             }
 
             @Override
             public void onCancelled(@NonNull DatabaseError error) {
-                Toast.makeText(Home.this, "Something went wrong!",Toast.LENGTH_LONG).show();
-                progressBar.setVisibility(View.GONE);
+                Toast.makeText(Cook.this, "Something went wrong!", Toast.LENGTH_LONG).show();
             }
         });
-
     }
 }
